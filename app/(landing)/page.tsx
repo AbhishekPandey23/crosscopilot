a039b873960f4c4d47f44b23d187d8ada1c5d4e0
Abhishek Pandey <abhishekpandey1160@gmail.com>
2025-11-10 18:09:31 +0530

    first commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import { HeroHeader } from '@/app/(landing)/_components/header';
+import HeroSection from '@/app/(landing)/_components/hero-section';
+
+export default function Home() {
+  return (
+    <div>
+      <HeroHeader />
+      <HeroSection />
+    </div>
+  );
+}
